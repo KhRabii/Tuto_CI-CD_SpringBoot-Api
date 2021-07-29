d6118ff17dfe96c68ed04663fe6b392d3f33e5b2
KhRabii <42442111+KhRabii@users.noreply.github.com>
2021-07-29 10:46:38 +0100

    Create tutoResourceTest.java

---
+++ src/test/java/com/cicd/pipline/TutospringBoot/Resource/tutoResourceTest.java
@@ -0,0 +1,40 @@
+package com.cicd.pipline.TutospringBoot.Resource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class tutoResourceTest {
+
+    @Autowired
+    HealthCheck mHealthCheck;
+  
+    @Autowired
+    DataResource mDataResource;
+
+    @Test
+    void health() {
+        assertEquals( "HEALTH CHECK OK!", mHealthCheck.healthCheck());
+    }
+
+    @Test
+    void version() {
+        assertEquals( "The actual version is 1.0.0", mDataResource.version());
+    }
+
+    @Test
+    void nationsLength() {
+        Integer nationsLength = mDataResource.getRandomNations().size();
+        assertEquals(10, nationsLength);
+    }
+
+    @Test
+    void currenciesLength() {
+        Integer currenciesLength = mDataResource.getRandomCurrencies().size();
+        assertEquals(20, currenciesLength);
+    }
+
+}
