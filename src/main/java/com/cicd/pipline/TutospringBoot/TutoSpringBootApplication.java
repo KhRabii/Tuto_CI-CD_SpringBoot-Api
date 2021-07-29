0f9e5a7a7353024fdd0da01dafb2d9bc139df4e9
KhRabii <42442111+KhRabii@users.noreply.github.com>
2021-07-29 10:41:01 +0100

    [first-commit]

---
+++ src/main/java/com/cicd/pipline/TutospringBoot/TutoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.cicd.pipline.TutospringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutoSpringBootApplication.class, args);
+	}
+
+}
